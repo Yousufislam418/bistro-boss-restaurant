b308f056a35d7463eb35f0de8ed461542e6525d6
Yousufislam418 <yousufislamaps@gmail.com>
2024-12-14 02:15:07 +0300

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+ return (
+  <section>
+    <div className="div">
+        <h1 className="font-bold text-red-500">Home</h1>
+    </div>
+  </section>
+ )
+}
+export default Home;
---
+++ src/Pages/Layout/Main/Main.jsx
@@ -0,0 +1,10 @@
+import Home from "../../Home/Home/Home";
+
+const Main = () => {
+ return (
+  <section>
+    <Home></Home>
+  </section>
+ )
+}
+export default Main;
